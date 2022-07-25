f961fd04af12460b3f12e556e2155d30f5dd5e8f
SAHIL0455 <sahilpawar1004@gmail.com>
2022-07-25 23:36:13 +0530

    ADDED: ability to retrive the date between the date of range

--- src/main/java/com/bridgelabz/DatabaseDemo.java
+++ src/main/java/com/bridgelabz/DatabaseDemo.java
@@ -14,10 +14,10 @@ public class DatabaseDemo {
             System.out.println("Driver is Loaded");
             connection = DriverManager.getConnection(jdbcURL, name, password);
             System.out.println("connection is successfully");
-            PreparedStatement preparedStatement = connection.prepareStatement("update payroll_service.employee_payroll set salary = 300005 where name = 'sahil'");
-            preparedStatement.execute();
-            ResultSet resultSet = preparedStatement.executeQuery("select * from payroll_service.employee_payroll");
-            System.out.println("update salary in Employee Payroll service");
+            Statement statement = connection.createStatement();
+            statement.execute("update payroll_service.employee_payroll set salary = 300005 where name = 'sahil'");
+            ResultSet resultSet = statement.executeQuery("select * from payroll_service.employee_payroll where start between cast('2022-06-01' as date) and date(now())");
+            System.out.println("Retrieve employee data from database :");
             while (resultSet.next()) {
                 System.out.println(resultSet.getInt("id") + " " + resultSet.getString("name")
                         + " " + resultSet.getString("start")+" "+resultSet.getDouble("salary"));
